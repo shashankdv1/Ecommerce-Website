683945d0ecfc433a16add01dc2efd776b7fca56d
shashankdv1 <shashankvazeka31@gmail.com>
2025-05-03 17:06:17 +0530

    Development under progress

--- src/Pages/AddItems.jsx
+++ src/Pages/AddItems.jsx
@@ -2,9 +2,28 @@ import React from "react";
 
 function AddItems()
 {
-    return(<div>
-        <h1>Add Items from here</h1>
-    </div>
+    return(
+    <div className="flex justify-center items-center">
+        <form className="flex flex-col">
+        <div className="flex items-center gap-4">
+            <label for="name">Name: </label>
+            <input className="flex-1 border-b-2"type="text" name="name" placeholder="Enter your Product name"></input>
+            </div>
+            <div className="flex items-center gap-4">
+            <label for="price">Price: </label>
+            <input type="number" className="p-2" name="price" placeholder="Enter your Product price"></input>
+            </div>
+            <div className="flex items-center gap-4">
+            <label for="category">Category: </label>
+            <input type="text" name="category" className="p-2" placeholder="Enter your Product Category"></input>
+            </div>
+            <div className="flex items-center gap-4">
+            <label for="description">Description</label>
+            <input type="text" name="description" placeholder="Enter your product description"></input>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        </div>
     );
 }
 
