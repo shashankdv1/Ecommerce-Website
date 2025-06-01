084ab8dd06504fccf9e6218700673bf31c1da0a9
shashankdv1 <shashankvazeka31@gmail.com>
2025-06-01 23:55:50 +0530

    Added RemoveItems Page to Website

---
+++ src/Pages/AdminDashboard.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useAdmin } from "../userContext";
+const AdminDashboard=()=>{
+    const navigate=useNavigate();
+     const {admin}=useAdmin();
+    const addItems=()=>{
+        navigate('/AddItems');
+}
+const removeItems=()=>{
+
+}
+ if(admin===null)
+            {
+               
+                return <p>You do not have priviliges to access this page</p>
+            } 
+    return(<>
+    <div class="flex-col">
+    <button onClick={addItems} class="w-16 h-16 bg-red-400">AddItems</button><br />
+    <button onClick={removeItems} class="w-16 h-16 bg-red-400 mt-7">RemoveItems</button>
+    </div>
+    </>);
+}
+
+export default AdminDashboard;
---
+++ src/Pages/RemoveItems.jsx
@@ -0,0 +1,11 @@
+const RemoveItems=()=>{
+    return(
+        <>
+        <form>
+            <label for="productId">Product Id: </label><input name="productId" type="number"></input>
+        </form>
+        </>
+    )
+}
+
+return RemoveItems;
